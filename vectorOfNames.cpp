20d07d7a39439104ced6fd2b7f81725186c87c40
d-howell <43057622+d-howell@users.noreply.github.com>
2018-09-25 13:57:51 -0500

    Formatted to for easier reading

--- vectorOfNames.cpp
+++ vectorOfNames.cpp
@@ -37,45 +37,54 @@ int main()
 
         do{
 
-        cout << "You may choose to Add, Change, Remove, or Show names!\n";
-        this_thread::sleep_for(chrono::milliseconds(1000));
-        cout << "Either enter your choice, or enter 'quit' to exit!\n>> ";
+            cout << "You may choose to Add, Change, Remove, or Show names!\n";
+            this_thread::sleep_for(chrono::milliseconds(1000));
+            cout << "Either enter your choice, or enter 'quit' to exit!\n>> ";
 
-        getline(cin,input);
-        for(int i = 0; i < input.size();++i) //Changes the string to all uppercase
-        {
-            input[i] = toupper(input[i]);
-        }
-
-        if (input == "ADD")
-        {   
-            cout << "You'd like to add a name? We can do that!\n";
-            cout << "What name would you like to add?\n>>";
-            getline(cin,input); //reusing input variable.
-            names.push_back(input);
-            break;
-        } else if (input == "CHANGE") {
-            break;
-        } else if (input == "REMOVE") { //remove names
-            cout << "Which name would you like to remove? This is case-sensitive!\n>>"; //warn player name is case sensitive
             getline(cin,input);
-            readIter = find(names.begin(),names.end(), input);
-            if(readIter != names.end())
+            for(int i = 0; i < input.size();++i) //Changes the string to all uppercase
             {
-                names.erase(readIter);
+                input[i] = toupper(input[i]);
             }
-            cout << endl << input << " has been removed!\n";
-            this_thread::sleep_for(chrono::milliseconds(1000));
-            break;
-        } else if (input == "SHOW") { //Since the names are listed at the beginning, to list a gain, break the do loop
-            break;
-        } else if (input == "QUIT") { //quit the program
-            break;
-        } else {
-            cout << "\nInvalid input! Try again.\n";
-            this_thread::sleep_for(chrono::milliseconds(1000));
-            continue;
-        }
+
+            if (input == "ADD")
+            {   
+                cout << "You'd like to add a name? We can do that!\n";
+                cout << "What name would you like to add?\n>>";
+                getline(cin,input); //reusing input variable.
+                names.push_back(input);
+                break;
+
+            } else if (input == "CHANGE") {
+
+                break;
+            } else if (input == "REMOVE") { //remove names
+
+                cout << "Which name would you like to remove? This is case-sensitive!\n>>"; //warn player name is case sensitive
+                getline(cin,input);
+                readIter = find(names.begin(),names.end(), input);
+                if(readIter != names.end())
+                {   
+                    names.erase(readIter);
+                }
+                cout << endl << input << " has been removed!\n";
+                this_thread::sleep_for(chrono::milliseconds(1000));
+                break;
+
+            } else if (input == "SHOW") { //Since the names are listed at the beginning, to list a gain, break the do loop
+
+                break;
+
+            } else if (input == "QUIT") { //quit the program
+
+                break;
+
+            } else {
+
+                cout << "\nInvalid input! Try again.\n";
+                this_thread::sleep_for(chrono::milliseconds(1000));
+                continue;
+            }
         }while(true);//do loop here to return when an invalid input is given
 
     }while(input != "QUIT"); //quit program
